262c61807e321534ea40150fe80df8c80c8c86e0
Paul Mason <paulthediyguy@gmail.com>
2015-10-28 15:50:44 -0400

    Better start/end validation. No more touchspin

--- app/models/video.rb
+++ app/models/video.rb
@@ -9,6 +9,7 @@ class Video < ActiveRecord::Base
   #
   validates :title, :presence => true
   validates :link, :presence => true, :url => true
+  validate :validate_start_end_time
 
   ##
   # Relastions
@@ -26,5 +27,13 @@ class Video < ActiveRecord::Base
   def set_sort_order
     order = show.videos.maximum(:sort_order) + 1 rescue 0
     write_attribute(:sort_order, order)
+  end
+
+  def validate_start_end_time
+    if start_time && end_time && start_time >= end_time
+      errors.add(:base, "Start At cannot be greater than End At")
+      return false
+    end
+    return true
   end
 end
