3df6a70a804f4edc4d874919dc980c86dd78de77
Paul Mason <paul.mason@kinetic-social.com>
2015-11-09 14:55:36 -0500

    Better handling of messaging and identifying each video preview

--- app/controllers/video_player_controller.rb
+++ app/controllers/video_player_controller.rb
@@ -1,30 +1,24 @@
 class VideoPlayerController < WebsocketRails::BaseController
   def load
-    begin
-      video = Video.find(message[:video_id])
-      trigger_success({ :message => video.api_video_id })
-    rescue ActiveRecord::RecordNotFound
-      trigger_failure({ :message => 'Video not found' })
-    rescue Exception => e
-      trigger_failure({ :message => e.to_s })
-    end
-    WebsocketRails[:video_player].trigger(:load, {:video_id => video.id })
+    player_id = message[:player_id]
+    video = Video.find(message[:video_id])
+    WebsocketRails[:video_player].trigger(:load, {:video_id => video.id, :player_id => message["player_id"] })
   end
 
   def ready
-    WebsocketRails[:video_player].trigger(:ready, { :video_id => message["video_id"], :video_key => message["video_key"]})
+    WebsocketRails[:video_player].trigger(:ready, { :video_id => message["video_id"], :player_id => message["player_id"] })
   end
 
   def play
-    WebsocketRails[:video_player].trigger(:play, { :video_id => message["video_id"], :video_key => message["video_key"]})
+    WebsocketRails[:video_player].trigger(:play, { :video_id => message["video_id"], :player_id => message["player_id"] })
   end
 
   def pause
-    WebsocketRails[:video_player].trigger(:pause, { :video_id => message["video_id"], :video_key => message["video_key"]})
+    WebsocketRails[:video_player].trigger(:pause, { :video_id => message["video_id"], :player_id => message["player_id"] })
   end
 
   def stop
-    WebsocketRails[:video_player].trigger(:stop, { :video_id => message["video_id"], :video_key => message["video_key"]})
+    WebsocketRails[:video_player].trigger(:stop, { :video_id => message["video_id"], :player_id => message["player_id"] })
   end
 
   def mute
