fd72eea7b312c663653447d97b7b632e79b46e06
Paul Mason <paul.mason@kinetic-social.com>
2015-11-09 15:20:39 -0500

    Allow login from multiple machines (same account)

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,6 @@ class User < ActiveRecord::Base
     user.email = auth.info.email
     user.name = auth.info.name
     user.profile_image = auth.info.image
-    user.password = "test" #Devise.friendly_token[0,20]
     user.save!
     user
   end
