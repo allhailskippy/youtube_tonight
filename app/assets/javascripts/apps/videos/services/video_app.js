1b45006dc0401d34eb35326c801ec607c90e714d
Paul Mason <paul.mason@kinetic-social.com>
2015-11-11 22:08:08 -0500

    Moving dispatcher/channel to common code to reduce open channels

--- app/assets/javascripts/apps/videos/services/video_app.js
+++ app/assets/javascripts/apps/videos/services/video_app.js
@@ -2,11 +2,24 @@
 (function(){
 'use strict';
 
-var VideoApp = function() {
+var VideoApp = function(ApplicationConstants) {
   var self = this;
+
+  self.dispatcher = null;
+  self.channel = null;
+
+  self.getDispatcher = function() {
+    self.dispatcher = self.dispatcher || new WebSocketRails(ApplicationConstants.WEBSOCKET_URL);
+    return self.dispatcher;
+  };
+
+  self.getChannel = function(channel_name) {
+    self.channel = self.channel || self.getDispatcher().subscribe(channel_name);
+    return self.channel;
+  };
 };
 
-VideoApp.$inject = [];
+VideoApp.$inject = ['ApplicationConstants'];
 
 angular.module('VideoApp')
        .service('VideoApp', VideoApp);
