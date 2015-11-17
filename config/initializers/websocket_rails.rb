e4e6d9eb32781683ada61d5945f6d9a0a3f826cb
Paul Mason <paulthediyguy@gmail.com>
2015-11-17 14:01:18 -0500

    Updating to get websockets to work

--- config/initializers/websocket_rails.rb
+++ config/initializers/websocket_rails.rb
@@ -28,7 +28,7 @@ WebsocketRails.setup do |config|
   # Uncomment and edit to point to a different redis instance.
   # Will not be used unless standalone or synchronization mode
   # is enabled.
-  # config.redis_options = {:host => 'localhost', :port => '6379'}
+  config.redis_options = {:host => 'panga.redistogo.com', :port => '9348', :user => 'redistogo', :password => '82624e10aa9d5c4914967d2b0d892927'}
 
   # By default, all subscribers in to a channel will be removed
   # when that channel is made private. If you don't wish active
