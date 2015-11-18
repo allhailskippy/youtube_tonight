0aaadab834a8641adcbd93a0783611c4d5ca54bc
Paul Mason <paulthediyguy@gmail.com>
2015-11-18 13:05:33 -0500

    Send current_time/duration with currently playing message

--- app/controllers/video_player_controller.rb
+++ app/controllers/video_player_controller.rb
@@ -10,11 +10,7 @@ class VideoPlayerController < WebsocketRails::BaseController
 
   events.each do |e|
     define_method("#{e}") do
-      WebsocketRails[:video_player].trigger(e, {
-        :video => message["video"],
-        :player_id => message["player_id"],
-        :sender_id => message["sender_id"]
-      })
+      WebsocketRails[:video_player].trigger(e, message)
     end
   end
 end
