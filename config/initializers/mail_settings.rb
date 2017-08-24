745820beaade072192649e29fcc0e7ba06c7f622
Paul Mason <paulthediyguy@gmail.com>
2017-08-24 17:44:56 -0400

    TID-53: Fix error when authorizing user

--- config/initializers/mail_settings.rb
+++ config/initializers/mail_settings.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'youtubetonight.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
   :authentication => :plain,
-  :user_name      => 'app44000026@heroku.com',
-  :password       => 'abcnyhhp2963',
-  :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
