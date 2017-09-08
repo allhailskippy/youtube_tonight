b32e21a1efcfc65a97aff67e82c64f3e3e4ef322
Paul Mason <paulthediyguy@gmail.com>
2017-09-07 21:00:02 -0400

    TID-92: Convert hosts to actual user objects

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,5 @@
 class Show < ActiveRecord::Base
-  attr_accessor :video_count
+  attr_accessor :video_count, :hosts
 
   ##
   # Validations
@@ -12,13 +12,34 @@ class Show < ActiveRecord::Base
   # Relationships
   #
   has_many :videos, dependent: :destroy, as: :parent
+  has_many :show_users
+  has_many :users, through: :show_users
+
+  ##
+  # Hooks
+  #
+  before_save :update_hosts
 
   ##
   # Methods
   #
   def self.as_json_hash
     {
+      include: :users,
       methods: [:video_count]
     }
+  end
+
+  def hosts
+    @hosts || users.collect(&:id).join(',')
+  end
+
+  # Deal with roles on update
+  def update_hosts
+    # Wipe out any existing roles
+    users.destroy_all
+    users.reload
+
+    users << User.where('id in (?)', hosts.split(',')).all
   end
 end
---
+++ db/migrate/20170908003451_create_show_users.rb
@@ -0,0 +1,32 @@
+class CreateShowUsers < ActiveRecord::Migration
+  def up
+    Show.reset_column_information
+
+    create_table :show_users do |t|
+      t.integer :show_id
+      t.integer :user_id
+      t.userstamps
+      t.timestamps
+    end
+
+    Show.all.each do |show|
+      show.users << User.find(show.hosts.split(','))
+      show.save
+    end
+
+    remove_column :shows, :hosts
+  end
+
+  def down
+    add_column :shows, :hosts, :string
+
+    Show.reset_column_information
+
+    Show.all.each do |show|
+      show.hosts = show.users.collect(&:id).join(',')
+      show.save
+    end
+      
+    drop_table :show_users
+  end
+end
